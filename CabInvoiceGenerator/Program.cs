a18a3e5ca416a43328e3061a3d3da41481006b79
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-06-14 07:40:25 +0530

    [Zeeshan] : Added main page for the program

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\t\t Welcome to Cab Invoice Generator Program!!");
+            Console.WriteLine("\t\t------------------------------------------- ");
+            Console.ResetColor();
+        }
+    }
+}
